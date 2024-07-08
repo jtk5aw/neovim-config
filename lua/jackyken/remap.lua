53a50510ba91dc2b18d6170860bb984caef17ae5
Jackson Kennedy <jacksontkennedy99@gmail.com>
2024-07-07 20:00:09 -0400

    Add <C-[]> mappings to switch windows

--- lua/jackyken/remap.lua
+++ lua/jackyken/remap.lua
@@ -3,3 +3,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+vim.keymap.set("n", "<leader>vs", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>hs", vim.cmd.split)
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
