4cf6914a1303346ae69df4eedba4f0b9a988c279
Jackson Kennedy <jacksonkennedy@Jacksons-MacBook-Pro.local>
2025-03-29 16:14:05 -0400

    doc: Fix previous commit to actually comment

--- after/plugin/neoformat.lua
+++ after/plugin/neoformat.lua
@@ -1,6 +1,6 @@
 vim.g.neoformat_try_node_exe = true
 
-// Note to self: For this to work prettier has to be globally installed
+-- Note to self: For this to work prettier has to be globally installed
 vim.api.nvim_create_autocmd({"BufWritePre"}, {
     pattern = {"*.js", "*.ts", "*.tsx", "*.jsx"},
     command = "Neoformat prettier",
